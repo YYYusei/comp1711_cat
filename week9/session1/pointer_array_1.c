0159159d936ab2320b3af4701293a1ec74f4e123
YYYusei <146363673+YYYusei@users.noreply.github.com>
2023-11-28 14:33:58 +0000

    week9s1

--- week9/session1/pointer_array_1.c
+++ week9/session1/pointer_array_1.c
@@ -5,13 +5,16 @@ int main()
     int vector[5] = {1,2,3,4,5};
 
     int* address;
+    int b;
 
     address = vector; 
+    address = &b;
 
     printf("\n \n address = %p\n", address);
 
     printf( " vector = %p\n\n",vector);
 
     printf ("address[0] = %d  ,  vector[0] = %d  \n\n",address[0],vector[0]);
+    printf("vector = %d\n", b);
 
 }
